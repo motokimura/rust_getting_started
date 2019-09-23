435eea5dc3d5b21675163267a8f7e3ac4506edda
Motoki Kimura <motoki.kimura.1990@gmail.com>
2019-09-23 21:09:58 +0900

    Add 4.3 primitive types

---
+++ 4_syntax_and_semantics/_4_3_primitive_types/src/main.rs
@@ -0,0 +1,65 @@
+fn main() {
+    // ブーリアン型
+    let x = true;
+    println!("{}", x);
+
+    let y: bool = false;
+    println!("{}", y);
+
+    // char
+    let x = 'x';
+    let two_hearts = '💕';
+
+    println!("{}", x);
+    println!("{}", two_hearts);
+
+    // 数値型
+    let x = 42;
+    let y = 1.0;
+
+    println!("{}", x);
+    println!("{}", y);
+
+    // 配列
+    let a = [1, 2, 3];
+    println!("{:?}", a);
+
+    let a = [0; 20];
+    println!("{:?}", a);
+    println!("{}", a.len());
+
+    let names = ["Graydon", "Brain", "Niko"];
+    println!("The second name is: {}", names[1]);
+
+    // スライス
+    let a = [0, 1, 2, 3, 4];
+    let complete = &a[..];
+    let middle = &a[1..4];
+
+    println!("{:?}", complete);
+    println!("{:?}", middle);
+
+    // タプル
+    let x = (1, "hello");
+    println!("{:?}", x);
+
+    let x: (i32, &str) = (1, "hello");
+    println!("{:?}", x);
+
+    let mut x = (1, 2);
+    println!("{:?}", x);
+    let y = (2, 3);
+    x = y;
+    println!("{:?}", x);
+
+    let (x, y, z) = (1, 2, 3);
+    println!("{}, {}, {}", x, y, z);
+
+    // 関数
+    let fx: fn(i32) -> i32 = foo;
+    println!("{}", fx(100));
+}
+
+fn foo(x: i32) -> i32 {
+    x
+}
